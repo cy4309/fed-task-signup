46e60e081b25fca48f9947160c7aa15b15582c44
cheng <apple@appledeMacBook-Pro.local>
2024-07-01 02:34:33 +0800

    add_regex

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -1,14 +1,94 @@
 <script setup>
+import { reactive } from 'vue'
 import BtnReturn from '@/components/BtnReturn.vue'
 import BtnBase from '@/components/BtnBase.vue'
-
+// import icons
 import IconGoogle from '@/assets/icons/IconGoogle.vue'
 import IconFacebook from '@/assets/icons/IconFacebook.vue'
 import IconTickInactive from '@/assets/icons/IconTickInactive.vue'
 import IconTickActive from '@/assets/icons/IconTickActive.vue'
 import IconWarningCircle from '@/assets/icons/IconWarningCircle.vue'
+import IconViewInactive from './../assets/icons/IconViewInactive.vue'
+import IconViewActive from './../assets/icons/IconViewActive.vue'
 
-const openUrl = (url) => window.open(url)
+// declair
+const state = reactive({
+  userData: {
+    firstname: '',
+    lastname: '',
+    email: '',
+    password: ''
+  },
+  isErrorMsg: false,
+  isShowHintMsg: false,
+  isPasswordVisible: false,
+  // validation
+  isPass8characters: false,
+  isPass1number: false,
+  errorArr: []
+})
+
+// to check if the field has error
+const hasError = (fieldName) => state.errorArr.includes(fieldName)
+const urlOpen = (url) => window.open(url)
+const toggleViewIcon = () => {
+  state.isPasswordVisible = !state.isPasswordVisible
+}
+
+const userSignup = () => {
+  // replace all the blanks
+  state.userData.firstname = state.userData.firstname.replace(/\s+/g, '')
+  state.userData.lastname = state.userData.lastname.replace(/\s+/g, '')
+  // regex
+  const nameRegex = /^[A-Za-z]+$/ // a-z || A-Z
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+  // reset errorArr
+  state.errorArr = []
+  // check if no null
+  for (const key of Object.keys(state.userData)) {
+    if (!state.userData[key]) state.errorArr.push(key)
+  }
+  for (const key of Object.keys(state.userData)) {
+    switch (key) {
+      case 'firstname':
+      case 'lastname': {
+        const isNameValid = nameRegex.test(state.userData[key])
+        state.isErrorMsg = !isNameValid
+        if (!isNameValid) {
+          state.errorArr.push(key)
+        }
+        break
+      }
+      case 'email': {
+        const isEmailValid = emailRegex.test(state.userData.email)
+        state.isErrorMsg = !isEmailValid
+        if (!isEmailValid) {
+          state.errorArr.push(key)
+        }
+        break
+      }
+      case 'password': {
+        // check password length: at least 8 characters
+        const isPasswordLengthRegex = state.userData.password.length >= 8
+        state.isPass8characters = isPasswordLengthRegex
+
+        // regex for password digits: at least 1 number
+        const isPasswordDigitRegex = /\d/.test(state.userData.password)
+        state.isPass1number = isPasswordDigitRegex
+
+        state.isErrorMsg = !isPasswordLengthRegex || !isPasswordDigitRegex
+        state.isShowHintMsg = true
+        if (!isPasswordLengthRegex || !isPasswordDigitRegex) {
+          state.errorArr.push(key)
+        }
+        break
+      }
+    }
+  }
+  state.isErrorMsg = state.errorArr.length > 0
+  state.isShowHintMsg = true
+}
 </script>
 
 <template>
@@ -20,7 +100,7 @@ const openUrl = (url) => window.open(url)
           <h3 class="card__title">Start from free</h3>
           <h2 class="card__title--important">Create an account</h2>
           <!-- Notification -->
-          <div class="card__notification">
+          <div class="card__notification" v-if="state.isErrorMsg == true">
             <IconWarningCircle class="icon" />
             <p>Please complete all the required fields to proceed.</p>
           </div>
@@ -29,13 +109,13 @@ const openUrl = (url) => window.open(url)
               label="Sign up with Google"
               :icon="IconGoogle"
               class="btn--google"
-              @click="openUrl('http://www.google.com')"
+              @click="urlOpen('http://www.google.com')"
             />
             <BtnBase
               label="Sign up with Facebook"
               :icon="IconFacebook"
               class="btn--fb"
-              @click="openUrl('http://www.facebook.com')"
+              @click="urlOpen('http://www.facebook.com')"
             />
           </div>
         </div>
@@ -46,18 +126,52 @@ const openUrl = (url) => window.open(url)
               <div class="form__line"></div>
             </div>
             <div class="form__input">
-              <input id="firstname" type="text" placeholder="First Name" />
-              <input id="lastname" type="text" placeholder="Last Name" />
-              <input id="emailaddress" type="email" placeholder="E-mail" />
-              <input id="password" type="password" placeholder="Password" />
+              <input
+                id="firstname"
+                type="text"
+                placeholder="First Name"
+                v-model="state.userData.firstname"
+                :class="{ error: hasError('firstname') }"
+              />
+              <input
+                id="lastname"
+                type="text"
+                placeholder="Last Name"
+                v-model="state.userData.lastname"
+                :class="{ error: hasError('lastname') }"
+              />
+              <input
+                id="emailaddress"
+                type="email"
+                placeholder="E-mail"
+                v-model="state.userData.email"
+                :class="{ error: hasError('email') }"
+              />
+              <div class="form__input--password">
+                <input
+                  id="password"
+                  placeholder="Password"
+                  v-model="state.userData.password"
+                  :type="state.isPasswordVisible ? 'text' : 'password'"
+                  :class="{ error: hasError('password') }"
+                />
+                <IconViewActive
+                  class="checkeye"
+                  v-if="state.isPasswordVisible"
+                  @click="toggleViewIcon"
+                />
+                <IconViewInactive class="checkeye" v-else @click="toggleViewIcon" />
+              </div>
             </div>
-            <div class="form__validation">
+            <div class="form__validation" v-if="state.isShowHintMsg">
               <div class="form__validation--hint">
-                <IconTickInactive class="icon" />
+                <IconTickActive class="icon" v-if="state.isPass8characters" />
+                <IconTickInactive v-else />
                 <p>8 Characters min</p>
               </div>
               <div class="form__validation--hint">
-                <IconTickInactive class="icon" />
+                <IconTickActive class="icon" v-if="state.isPass1number" />
+                <IconTickInactive v-else />
                 <p>One number</p>
               </div>
             </div>
@@ -69,7 +183,9 @@ const openUrl = (url) => window.open(url)
                   Notification settings.
                 </label>
               </div>
-              <button type="submit" class="btn btn--submit">Create an Free Account!</button>
+              <button type="submit" class="btn btn--submit" @click="userSignup">
+                Create an Free Account!
+              </button>
               <p class="form__submit--link">
                 Already have an account?
                 <router-link to="/login" class="btn--return--login">Log in</router-link>
@@ -173,9 +289,23 @@ input {
 input[type='text'] {
   width: 190px;
 }
-input[type='email'],
-input[type='password'] {
+input[type='email'] {
   width: 400px;
+}
+input.error {
+  border: 1px solid red;
+}
+#password {
+  width: 400px;
+}
+.form__input--password {
+  position: relative;
+}
+.checkeye {
+  position: absolute;
+  top: 50%;
+  right: 10px;
+  transform: translateY(-70%);
 }
 .form__validation {
   display: flex;
